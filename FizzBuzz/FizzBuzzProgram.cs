2031bb8a44ae6614bc621498b4ea7048f19d03df
Ainsley Crawford <ainsley.c@hotmail.co.uk>
2019-12-16 17:27:16 +0000

    FizzBuzz program complete

---
+++ FizzBuzz/FizzBuzzProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzMethod
+{
+    public class FBClass
+    {
+        public string FizzBuzzProgram(int value)
+        {
+            if (value % 3 == 0 & value % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (value % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (value % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+    }
+}
