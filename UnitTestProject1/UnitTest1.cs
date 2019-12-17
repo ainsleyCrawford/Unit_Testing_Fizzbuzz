0c9cf2cf82cc7c779a0c4dd4221073bc938ece6a
Ainsley Crawford <ainsley.c@hotmail.co.uk>
2019-12-17 12:20:01 +0000

    Four tests pass

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -2,16 +2,35 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FizzBuzz;
 
-namespace UnitTestProject1
+namespace FizzBuzzUnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTests
     {
+        FizzBuzzClass fb = new FizzBuzzClass();
         [TestMethod]
-        public void TestMethod1()
+        public void TestFizz()
         {
-            FizzBuzzClass fb = new FizzBuzzClass();
+            //Inputting a multiple of 3
             Assert.AreEqual("Fizz", fb.FizzBuzzProgram(42));
+        }
+        [TestMethod]
+        public void TestBuzz()
+        {
+            //Inputting a multiple of 5
+            Assert.AreEqual("Buzz", fb.FizzBuzzProgram(20));
+        }
+        [TestMethod]
+        public void TestFizzBuzz()
+        {
+            //Inputting a common multiple of 3 and 5
+            Assert.AreEqual("FizzBuzz", fb.FizzBuzzProgram(60));
+        }
+        [TestMethod]
+        public void TestIntegers()
+        {
+            //Inputting a number that is neither a multiple of 3 nor 5
+            Assert.AreEqual("11", fb.FizzBuzzProgram(11));
         }
     }
 }
